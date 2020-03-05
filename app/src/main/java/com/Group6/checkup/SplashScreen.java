c52b766ea29a4c589807a3f2fbd10f5898773157
IR0NM9N <60836693+IR0NM9N@users.noreply.github.com>
2020-03-05 14:12:20 -0800

    splash screen commit

---
+++ app/src/main/java/com/Group6/checkup/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.Group6.checkup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ImageView logo = findViewById(R.id.img_logo);
+
+        /* Animation will load the animation effect created in the
+          transition.xml file*/
+        Animation animation = AnimationUtils.loadAnimation
+                (this,R.anim.transition);
+        logo.startAnimation(animation);
+
+        //Timertask to end the SplashScreen activity and start MainActivity.
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashScreen.this,
+                        MainActivity.class));
+            }
+        };
+
+        // timer will schedule the MainActivity after a delay of 5seconds.
+        Timer time = new Timer();
+        time.schedule(task,5000);
+    }
+}
