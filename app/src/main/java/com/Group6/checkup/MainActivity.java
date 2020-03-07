12790be81cad4df5f67044159a66da02fe736b5d
IR0NM9N <60836693+IR0NM9N@users.noreply.github.com>
2020-03-06 22:37:43 -0800

    Created styles for the textViews and buttons. Also additional xml files to support the style.xml file.

--- app/src/main/java/com/Group6/checkup/MainActivity.java
+++ app/src/main/java/com/Group6/checkup/MainActivity.java
@@ -1,5 +1,6 @@
 package com.Group6.checkup;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient));
+        }*/
     }
 }
