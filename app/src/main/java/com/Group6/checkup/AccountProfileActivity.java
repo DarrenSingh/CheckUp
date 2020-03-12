0db58749d8fedf57c929393339836692939b28ab
Darren Singh <singh.darren0@gmail.com>
2020-03-11 17:50:30 -0700

    patient activity layout designs completed, activity java classes created
    strings have been hardcoded into the layout.xml files

---
+++ app/src/main/java/com/Group6/checkup/AccountProfileActivity.java
@@ -0,0 +1,14 @@
+package com.Group6.checkup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_profile);
+    }
+}
---
+++ app/src/main/java/com/Group6/checkup/PatientEditProfileActivity.java
@@ -0,0 +1,14 @@
+package com.Group6.checkup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientEditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_edit_profile);
+    }
+}
