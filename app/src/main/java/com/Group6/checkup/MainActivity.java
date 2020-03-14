4a47c446126332dcfaabb0ad588f0841b67e33c2
Darren Singh <singh.darren0@gmail.com>
2020-03-13 21:53:35 -0700

    added buttons to toggle between actor activities during testing

--- app/src/main/java/com/Group6/checkup/MainActivity.java
+++ app/src/main/java/com/Group6/checkup/MainActivity.java
@@ -3,7 +3,10 @@ package com.Group6.checkup;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
         if(actionBar != null){
             actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.gradient));
         }*/
+
+        Button login = findViewById(R.id.button);
+        Button admin = findViewById(R.id.btn_main_admin);
+        Button cashier = findViewById(R.id.btn_main_cashier);
+        Button doctor = findViewById(R.id.btn_main_doctor);
+        Button patient = findViewById(R.id.btn_main_patient);
+
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,AdminActivity.class));
+            }
+        });
+
+
     }
 }
