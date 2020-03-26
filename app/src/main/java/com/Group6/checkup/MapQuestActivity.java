961e6046d4f5ae3427bb4bf59201e4fc257b274f
Darren Singh <singh.darren0@gmail.com>
2020-03-26 16:46:00 -0700

    basic mapview implemented

---
+++ app/src/main/java/com/Group6/checkup/MapQuestActivity.java
@@ -0,0 +1,51 @@
+package com.Group6.checkup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapquest.mapping.MapQuest;
+import com.mapquest.mapping.maps.MapView;
+
+public class MapQuestActivity extends Activity {
+    private MapView mMapView;
+    private MapboxMap mMapboxMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapQuest.start(getApplicationContext());
+        setContentView(R.layout.activity_map_quest);
+
+        mMapView = findViewById(R.id.mapquestMapView);
+
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                mMapboxMap = mapboxMap;
+                mMapView.setStreetMode();
+            }
+        });
+
+    }
+
+    @Override
+    public void onResume()
+    { super.onResume(); mMapView.onResume(); }
+
+    @Override
+    public void onPause()
+    { super.onPause(); mMapView.onPause(); }
+
+    @Override
+    protected void onDestroy()
+    { super.onDestroy(); mMapView.onDestroy(); }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    { super.onSaveInstanceState(outState); mMapView.onSaveInstanceState(outState); }
+}
