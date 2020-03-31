7fb461c61e2d89bd51aa03d6aacd3eb6e0224111
DarrenSingh <Singh.Darren0@gmail.com>
2020-03-30 21:01:26 -0700

    bug fix - database async seeding refactored to serperate class
    readable database called on DatabaseHelper getInstance() if instance is null to allow the override OnCreate() method to create tables and seed the database

---
+++ app/src/main/java/com/Group6/checkup/DatabasePackage/AsyncDatabaseSeeder.java
@@ -0,0 +1,88 @@
+package com.Group6.checkup.DatabasePackage;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.Group6.checkup.Entities.Admin;
+import com.Group6.checkup.Entities.Appointment;
+import com.Group6.checkup.Entities.Cashier;
+import com.Group6.checkup.Entities.Doctor;
+import com.Group6.checkup.Entities.Invoice;
+import com.Group6.checkup.Entities.OnlineHelp;
+import com.Group6.checkup.Entities.OnlineHelpReply;
+import com.Group6.checkup.Entities.Patient;
+import com.Group6.checkup.Utils.Dao.AdminDao;
+import com.Group6.checkup.Utils.Dao.AppointmentDao;
+import com.Group6.checkup.Utils.Dao.CashierDao;
+import com.Group6.checkup.Utils.Dao.DoctorDao;
+import com.Group6.checkup.Utils.Dao.InvoiceDao;
+import com.Group6.checkup.Utils.Dao.OnlineHelpDao;
+import com.Group6.checkup.Utils.Dao.OnlineHelpReplyDao;
+import com.Group6.checkup.Utils.Dao.PatientDao;
+
+public class AsyncDatabaseSeeder extends AsyncTask<Void,Void,Void> {
+
+    private AdminDao adminDao;
+    private CashierDao cashierDao;
+    private DoctorDao doctorDao;
+    private AppointmentDao appointmentDao;
+    private InvoiceDao invoiceDao;
+    private OnlineHelpDao onlineHelpDao;
+    private OnlineHelpReplyDao onlineHelpReplyDao;
+    private PatientDao patientDao;
+
+    public AsyncDatabaseSeeder() {
+
+        adminDao = new AdminDao(null);
+        cashierDao = new CashierDao(null);
+        doctorDao = new DoctorDao(null);
+        appointmentDao = new AppointmentDao(null);
+        invoiceDao = new InvoiceDao(null);
+        onlineHelpDao = new OnlineHelpDao(null);
+        onlineHelpReplyDao = new OnlineHelpReplyDao(null);
+        patientDao = new PatientDao(null);
+
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+
+            //Create Admin Users
+            this.adminDao.insert(new Admin("Super","User", "A001", "root"));
+
+            //Create Cashier Users
+            this.cashierDao.insert(new Cashier("Rachel","Green","C001","password",1));
+
+            //Create Doctor Users
+            this.doctorDao.insert(new Doctor("Micheal","Scarn","15122 72 Ave, Surrey, BC V3S 2G2","D001","password","604-555-2144","scarn@medicine.com",1));
+            this.doctorDao.insert(new Doctor("Deandre","Jordan","6844 King George Blvd, Surrey, BC V3W 4Z9","D002","password","604-555-2143","jordan@medicine.com",1));
+            this.doctorDao.insert(new Doctor("Elaine","Jackson","615300 105 Avenue, Surrey, BC","D003","password","604-555-2543","elaine@medicine.com",1));
+            this.doctorDao.insert(new Doctor("Robin","Lopez","4731 King George Blvd, Surrey, BC V3W 4Z9","D004","password","604-555-2343","lopez@medicine.com",1));
+
+            //Create Patient Users
+            this.patientDao.insert(new Patient("Jane","Doe","1234 Fake St","P001","password",true,"604-555-1234",123000998,"janedoe@mail.com",1));
+            this.patientDao.insert(new Patient("John","Doe","123 Fake St","P002","password1",true,"604-555-1234",123000999,"johndoe@mail.com",1));
+
+            //Create Appointments
+            this.appointmentDao.insert(new Appointment(System.currentTimeMillis(), 1,1));
+            this.appointmentDao.insert(new Appointment(System.currentTimeMillis()-3000, 1,1));
+
+            //Create Invoices
+            this.invoiceDao.insert(new Invoice(29.99, "May,20,2020", "paid", System.currentTimeMillis(), 1, 1, 1, 1));
+            this.invoiceDao.insert(new Invoice(19.99, "May,23,2020", "paid", System.currentTimeMillis(), 1, 1, 1, 2));
+
+            //Create Online Help
+            this.onlineHelpDao.insert(new OnlineHelp("Test Message1","Lorem Ipsum",System.currentTimeMillis()-8000,1,1));
+            this.onlineHelpDao.insert(new OnlineHelp("Test Message2","Lorem Ipsum",System.currentTimeMillis()-5000,1,2,1));
+
+            //Create Online Help
+            this.onlineHelpReplyDao.insert(new OnlineHelpReply("Test Reply","Lorem Ipsum",System.currentTimeMillis(),2));
+
+            Log.e("DATABASE HELPER","SEEDING COMPLETE");
+
+            return null;
+
+    }
+}
--- app/src/main/java/com/Group6/checkup/DatabasePackage/DatabaseHelper.java
+++ app/src/main/java/com/Group6/checkup/DatabasePackage/DatabaseHelper.java
@@ -5,16 +5,26 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.Group6.checkup.Entities.Admin;
+import com.Group6.checkup.Entities.Appointment;
 import com.Group6.checkup.Entities.Cashier;
 import com.Group6.checkup.Entities.Doctor;
+import com.Group6.checkup.Entities.Invoice;
+import com.Group6.checkup.Entities.OnlineHelp;
+import com.Group6.checkup.Entities.OnlineHelpReply;
 import com.Group6.checkup.Entities.Patient;
+import com.Group6.checkup.R;
 import com.Group6.checkup.Utils.Dao.AdminDao;
+import com.Group6.checkup.Utils.Dao.AppointmentDao;
 import com.Group6.checkup.Utils.Dao.CashierDao;
 import com.Group6.checkup.Utils.Dao.DoctorDao;
+import com.Group6.checkup.Utils.Dao.InvoiceDao;
+import com.Group6.checkup.Utils.Dao.OnlineHelpDao;
+import com.Group6.checkup.Utils.Dao.OnlineHelpReplyDao;
 import com.Group6.checkup.Utils.Dao.PatientDao;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -24,6 +34,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static DatabaseHelper getInstance(Context context) {
         if (sInstance == null) {
             sInstance = new DatabaseHelper(context);
+            sInstance.getReadableDatabase();
         }
         return sInstance;
     }
@@ -44,7 +55,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL(DatabaseEntries.SQL_ONLINE_HELP_REPLY_TABLE_CREATE_ENTRIES);
         db.execSQL(DatabaseEntries.SQL_ONLINE_HELP_TABLE_CREATE_ENTRIES);
 
-        new DatabaseSeedAsync().execute();
+        new AsyncDatabaseSeeder().execute();
+
     }
 
     @Override
@@ -74,38 +86,54 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
 
-    private class DatabaseSeedAsync extends AsyncTask<Void,Void,Void> {
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            //Create Admin Users
-            AdminDao adminDao = new AdminDao(null);
-            adminDao.insert(new Admin("Super","User", "A001", "root"));
-
-            //Create Cashier Users
-            CashierDao cashierDao = new CashierDao(null);
-            cashierDao.insert(new Cashier("Rachel","Green","C001","password",1));
-
-            //Create Doctor Users
-            DoctorDao doctorDao = new DoctorDao(null);
-            doctorDao.insert(new Doctor("Micheal","Scarn","322 Sydney Way","D001","password","604-555-2144","scarn@medicine.com",1));
-
-            //Create Patient Users
-            PatientDao patientDao = new PatientDao(null);
-            patientDao.insert(new Patient("Jane","Doe","1234 Fake St","P001","password",true,"604-555-1234",123000998,"janedoe@mail.com",1));
-            patientDao.insert(new Patient("John","Doe","123 Fake St","P002","password1",true,"604-555-1234",123000999,"johndoe@mail.com",1));
-
-
-            //Create Messages
+//    private class DatabaseSeedAsync extends AsyncTask<Void,Void,Void> {
+//        @Override
+//        protected Void doInBackground(Void... voids) {
+//
+//            //Create Admin Users
+//            AdminDao adminDao = new AdminDao(null);
+//            adminDao.insert(new Admin("Super","User", "A001", "root"));
+//
+//            //Create Cashier Users
+//            CashierDao cashierDao = new CashierDao(null);
+//            cashierDao.insert(new Cashier("Rachel","Green","C001","password",1));
+//
+//            //Create Doctor Users
+//            DoctorDao doctorDao = new DoctorDao(null);
+//            doctorDao.insert(new Doctor("Micheal","Scarn","15122 72 Ave, Surrey, BC V3S 2G2","D001","password","604-555-2144","scarn@medicine.com",1));
+//            doctorDao.insert(new Doctor("Deandre","Jordan","6844 King George Blvd, Surrey, BC V3W 4Z9","D002","password","604-555-2143","jordan@medicine.com",1));
+//            doctorDao.insert(new Doctor("Elaine","Jackson","615300 105 Avenue, Surrey, BC","D003","password","604-555-2543","elaine@medicine.com",1));
+//            doctorDao.insert(new Doctor("Robin","Lopez","4731 King George Blvd, Surrey, BC V3W 4Z9","D004","password","604-555-2343","lopez@medicine.com",1));
+//
+//            //Create Patient Users
+//            PatientDao patientDao = new PatientDao(null);
+//            patientDao.insert(new Patient("Jane","Doe","1234 Fake St","P001","password",true,"604-555-1234",123000998,"janedoe@mail.com",1));
+//            patientDao.insert(new Patient("John","Doe","123 Fake St","P002","password1",true,"604-555-1234",123000999,"johndoe@mail.com",1));
+//
+//            //Create Appointments
+//            AppointmentDao appointmentDao = new AppointmentDao(null);
+//            appointmentDao.insert(new Appointment(System.currentTimeMillis(), 1,1));
+//            appointmentDao.insert(new Appointment(System.currentTimeMillis()-3000, 1,1));
+//
+//            //Create Invoices
+//            InvoiceDao invoiceDao = new InvoiceDao(null);
+//            invoiceDao.insert(new Invoice(29.99, "May,20,2020", "paid", System.currentTimeMillis(), 1, 1, 1, 1));
+//            invoiceDao.insert(new Invoice(19.99, "May,23,2020", "paid", System.currentTimeMillis(), 1, 1, 1, 2));
+//
+//            //Create Online Help
 //            OnlineHelpDao onlineHelpDao = new OnlineHelpDao(null);
-            //TODO look into foreign key constraint error
-//            onlineHelpDao.insert(new OnlineHelp("Test Message1","Lorem Ipsum","Jan 23, 2020",1,1));
-//            onlineHelpDao.insert(new OnlineHelp("Test Message2","Lorem Ipsum","Jan 24, 2020",1,1));
-//            onlineHelpDao.insert(new OnlineHelp("Test Message3","Lorem Ipsum","Jan 27, 2020",1,1));
-
-            return null;
-        }
-    }
+//            onlineHelpDao.insert(new OnlineHelp("Test Message1","Lorem Ipsum",System.currentTimeMillis()-8000,1,1));
+//            onlineHelpDao.insert(new OnlineHelp("Test Message2","Lorem Ipsum",System.currentTimeMillis()-5000,1,2,1));
+//
+//            //Create Online Help
+//            OnlineHelpReplyDao onlineHelpReplyDao = new OnlineHelpReplyDao(null);
+//            onlineHelpReplyDao.insert(new OnlineHelpReply("Test Reply","Lorem Ipsum",System.currentTimeMillis(),2));
+//
+//            Log.e("DATABASE HELPER","SEEDING COMPLETE");
+//
+//            return null;
+//        }
+//    }
 
 
 }
